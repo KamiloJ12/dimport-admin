7f8dc2a306bf1d3faedee72b863f5feb5cccd7dd
KamiloJ12 <camilo12052000@hotmail.com>
2022-05-02 18:56:29 -0500

    initial heroku deploy

--- sever.js
+++ sever.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/dimport-admin'));
+app.use(express.static(__dirname + '/dist/Admin'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/dimport-admin/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/Admin/index.html'));
 });
 
 // default Heroku port
