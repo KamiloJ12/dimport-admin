7c78f1604f4be1eec8a5166d635b995cf88809fc
KamiloJ12 <camilo12052000@hotmail.com>
2022-05-02 21:20:19 -0500

    initial heroku deploy

--- sever.js
+++ sever.js
@@ -1,14 +1,15 @@
-const path = require('path');
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// Serve static files
-app.use(express.static(__dirname + '/dist/dimport-admin'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/dimport-admin'));
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/dimport-admin/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/dimport-admin/'}),
+);
 
-// default Heroku port
-app.listen(process.env.PORT || 5000);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
